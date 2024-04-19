ca19596cf574b883daf5866ee3364dfbdfbb2501
IvanKot <wocker@i.ua>
2024-04-19 16:20:04 +0300

    feat: version 1

---
+++ Runtime/AndroidBuildSettingEditorWindow.cs
@@ -0,0 +1,252 @@
+﻿#if UNITY_EDITOR
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using UnityEditor;
+using UnityEditorInternal;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace AndroidBuildSettings.Runtime
+{
+
+    public class AndroidBuildSettingEditorWindow: EditorWindow
+    {
+        [SerializeField] public int index = 0;
+        [SerializeField] public string buildFilename = "MyApplication";
+        [SerializeField] public bool developmentBuild;
+        [SerializeField] public bool appBundleBuild;
+        [SerializeField] public string outputFileDirectory = "C:/UnityBuilds";
+        
+        [SerializeField] List<string> devicesOptions = new List<string>() {"None"};
+        [SerializeField] List<string> Listas = new List<string>();
+        [SerializeField] public static string MyPhones;
+
+        [SerializeField] public int version1, version2, version3, bundleVersion = 0;
+        [SerializeField] public bool appVersionAutoincrement, bundleVersionAutoincrement = true;
+        [SerializeField] public bool deletePreviousBuild = true;
+        [SerializeField] public string previousBuildPath;
+
+        private static AndroidBuildSettingEditorWindow wnd;
+
+        [MenuItem("Window/Android Build Settings")]
+        public static void ShowExample()
+        {
+            wnd = GetWindow<AndroidBuildSettingEditorWindow>(false, "Android Build Settings", true);
+            wnd.minSize = new Vector2(450, 400);
+        }
+        
+        private void OnInspectorUpdate()
+        {
+            Repaint();
+        }
+        
+        protected void OnEnable ()
+        {
+            // Here we retrieve the data if it exists or we save the default field initialisers we set above
+            var data = EditorPrefs.GetString("AndroidBuildSettingEditorWindow", JsonUtility.ToJson(this, false));
+            // Then we apply them to this window
+            JsonUtility.FromJsonOverwrite(data, this);
+        }
+
+        private void OnDisable()
+        {
+            // We get the Json data
+            var data = JsonUtility.ToJson(this, false);
+            // And we save it
+            EditorPrefs.SetString("AndroidBuildSettingEditorWindow", data);
+        }
+
+        public string GetVersion()
+        {
+            return version1.ToString()+ "."+ version2.ToString()+ "."+ version3.ToString();
+        }
+        
+        public string GetBuildLocation()
+        {
+            return Path.Combine(outputFileDirectory, buildFilename + '-' + GetVersion() + ".apk");
+        }
+
+        public void OnGUI()
+        {
+            int uniformPadding = 10;
+            RectOffset padding = new RectOffset(uniformPadding, uniformPadding, uniformPadding, uniformPadding);
+//Builds Layout Area from padding values
+            Rect area = new Rect(padding.right, padding.top, position.width - (padding.right + padding.left), position.height - (padding.top + padding.bottom));
+ 
+            GUILayout.BeginArea(area);
+// Your GUI Code
+
+            //GUILayout.FlexibleSpace();
+            GUILayout.Space(15);
+            
+            EditorGUILayout.BeginHorizontal();
+            Texture2D LogoTex = (Texture2D)Resources.Load("android"); //don't put png
+            GUILayout.Label(LogoTex, EditorStyles.iconButton);
+
+            GUILayout.Label("Android", EditorStyles.boldLabel);
+            EditorGUILayout.EndHorizontal();
+            
+            GUILayout.Space(15);
+            
+            EditorGUILayout.BeginHorizontal();
+            buildFilename = EditorGUILayout.TextField("Build file name", buildFilename);
+            GUILayout.Label('-'+GetVersion()+".apk");
+            EditorGUILayout.EndHorizontal();
+            
+            EditorGUILayout.BeginHorizontal();
+            developmentBuild = EditorGUILayout.Toggle("Development build", developmentBuild);
+            EditorGUILayout.EndHorizontal();
+            
+            EditorGUILayout.BeginHorizontal();
+            appBundleBuild = EditorGUILayout.Toggle("Build App Bundle (Google Play)", appBundleBuild);
+            EditorGUILayout.EndHorizontal();
+            
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.TextField("Output file directory", outputFileDirectory);
+            if (GUILayout.Button("Browse", GUILayout.Width(60)))
+            {
+                outputFileDirectory = EditorUtility.OpenFolderPanel("Select output directory", "", "");
+            }
+            EditorGUILayout.EndHorizontal();
+            
+            EditorGUILayout.BeginHorizontal();
+            index = EditorGUILayout.Popup("Run device", index, devicesOptions.ToArray());
+            if (GUILayout.Button("Refresh", GUILayout.Width(60)))
+            {
+                devicesOptions = new List<string>() {"None"};
+                Listas = new List<string>();
+                index = 0;
+                
+                ProcessStartInfo psi = new ProcessStartInfo();
+                
+                psi.FileName = Path.Combine(EditorPrefs.GetString("AndroidSdkRoot"), "platform-tools/adb.exe");
+                psi.Arguments = "devices -l";
+                psi.RedirectStandardOutput = true;
+                psi.RedirectStandardError = true;
+                psi.RedirectStandardInput = true;
+                psi.UseShellExecute = false;
+                psi.CreateNoWindow = true;
+
+                Process p = new Process();
+                p.StartInfo = psi;
+                p.OutputDataReceived += p_DataReceived;
+                p.EnableRaisingEvents = true;
+                p.Start();
+                p.BeginOutputReadLine();
+                p.WaitForExit();
+                
+                foreach (var x in Listas)
+                {
+                    devicesOptions.Add(x);
+                }
+                
+                Repaint();
+            }
+            EditorGUILayout.EndHorizontal();
+            
+            EditorGUILayout.BeginHorizontal();
+            version1 = EditorGUILayout.IntField("App Version", version1);
+            GUILayout.Label(".");
+            version2 = EditorGUILayout.IntField(version2);
+            GUILayout.Label(".");
+            version3 = EditorGUILayout.IntField(version3);
+            GUILayout.Space(15);
+            appVersionAutoincrement = GUILayout.Toggle(appVersionAutoincrement, "Autoincrement");
+            EditorGUILayout.EndHorizontal();
+            
+            EditorGUILayout.BeginHorizontal();
+            bundleVersion = EditorGUILayout.IntField("Bundle Version", bundleVersion);
+            GUILayout.Space(15);
+            bundleVersionAutoincrement = GUILayout.Toggle(bundleVersionAutoincrement, "Autoincrement");
+            EditorGUILayout.EndHorizontal();
+            
+            EditorGUILayout.BeginHorizontal();
+            deletePreviousBuild = EditorGUILayout.Toggle("Delete Previous Build", deletePreviousBuild);
+            EditorGUILayout.EndHorizontal();
+            
+            GUILayout.FlexibleSpace();
+            //GUILayout.Space(60);
+            
+            GUILayout.Label("The output file path after build:", EditorStyles.miniLabel);
+            
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.HelpBox(GetBuildLocation(), MessageType.None);
+            if (GUILayout.Button("Open", EditorStyles.miniButton, GUILayout.Width(45)))
+            {
+                EditorUtility.RevealInFinder(GetBuildLocation());
+            }
+            EditorGUILayout.EndHorizontal();
+            
+            GUILayout.Space(15);
+            
+            EditorGUILayout.BeginHorizontal();
+            if (GUILayout.Button("Player Settings..."))
+            {
+                SettingsService.OpenProjectSettings("Project/Player");
+            }
+            GUILayout.FlexibleSpace();
+            if (GUILayout.Button("Build", GUILayout.Width(100)))
+            {
+                AndroidBuilder.Build(GetBuildLocation(), developmentBuild, "None", GetVersion(), bundleVersion, HandleVersion, appBundleBuild);
+            }
+            if (GUILayout.Button("Build And Run", GUILayout.Width(100)))
+            {
+                AndroidBuilder.Build(GetBuildLocation(), developmentBuild, devicesOptions[index], GetVersion(), bundleVersion, HandleVersion, appBundleBuild);
+            }
+            EditorGUILayout.EndHorizontal();
+            
+            
+            GUILayout.EndArea();
+        }
+        
+        void p_DataReceived(object sender, DataReceivedEventArgs e)
+        {
+            // Manipulate received data here
+            MyPhones = e.Data.ToString();
+            // UnityEngine.Debug.Log(MyPhones);
+            if (e.Data.Contains("device") && !e.Data.Contains("List"))
+            {
+                int index = MyPhones.LastIndexOf("model:") + 6;
+                if (index > 0)
+                {
+                    string serialNumber = MyPhones.Split(' ')[0];
+                    MyPhones = MyPhones.Substring(index);
+                    MyPhones = MyPhones.Substring(0, MyPhones.IndexOf("device"));
+                    Listas.Add(MyPhones+ $"({serialNumber})");
+                }
+            }
+
+            // if no devices, then there will be only "List of devices attached: "
+        }
+
+        public void HandleVersion()
+        {
+            if (previousBuildPath != null && deletePreviousBuild && File.Exists(previousBuildPath))
+            {
+                File.Delete(previousBuildPath);
+            }
+            previousBuildPath = GetBuildLocation();
+            
+            if (appVersionAutoincrement)
+            {
+                version3++;
+            }
+
+            if (bundleVersionAutoincrement)
+            {
+                bundleVersion++;
+            }
+            
+            // We get the Json data
+            var data = JsonUtility.ToJson(this, false);
+            // And we save it
+            EditorPrefs.SetString("AndroidBuildSettingEditorWindow", data);
+        }
+    }
+    
+    
+}
+#endif
---
+++ Runtime/AndroidBuilder.cs
@@ -0,0 +1,89 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditor.Build.Reporting;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using Debug = UnityEngine.Debug;
+
+namespace AndroidBuildSettings.Runtime
+{
+    public class AndroidBuilder
+    {
+        public static void Build(string locationPath, bool developmentBuild, string buildAndRun, string bundleVersion, int bundleVersionCode, Action HandleVersion, bool buildAppBundle)
+        {
+            List<string> scenes = new List<string>();
+            foreach (var scene in EditorBuildSettings.scenes)
+            {
+                if(scene.enabled)
+                    scenes.Add(scene.path);
+            }
+
+
+            BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+            buildPlayerOptions.scenes = scenes.ToArray();
+            buildPlayerOptions.locationPathName = locationPath;
+            buildPlayerOptions.target = BuildTarget.Android;
+            buildPlayerOptions.options = BuildOptions.None;
+            EditorUserBuildSettings.buildAppBundle = buildAppBundle;
+            if (developmentBuild)
+            {
+                buildPlayerOptions.options = BuildOptions.Development;
+            }
+            
+            PlayerSettings.bundleVersion = bundleVersion;
+            PlayerSettings.Android.bundleVersionCode = bundleVersionCode;
+
+            BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+            BuildSummary summary = report.summary;
+
+            if (summary.result == BuildResult.Succeeded)
+            {
+                Debug.Log("BuildPlayerOptions\n"
+                          + "Scenes: " + string.Join(",", buildPlayerOptions.scenes) + "\n"
+                          + "Build location: " + buildPlayerOptions.locationPathName + "\n"
+                          + "Options: " + buildPlayerOptions.options + "\n"
+                          + "Target: " + buildPlayerOptions.target);
+                Debug.Log("Build succeeded: " + summary.totalSize + " bytes");
+
+                if (buildAndRun != "None")
+                {
+                    ProcessStartInfo psi = new ProcessStartInfo();
+                
+                    psi.FileName = Path.Combine(EditorPrefs.GetString("AndroidSdkRoot"), "platform-tools/adb.exe");
+                    psi.Arguments = $"-s {buildAndRun.Split(new[] { '(', ')' }, StringSplitOptions.RemoveEmptyEntries)[1]} install -r {locationPath}";
+                    psi.RedirectStandardOutput = true;
+                    psi.RedirectStandardError = true;
+                    psi.RedirectStandardInput = true;
+                    psi.UseShellExecute = false;
+                    psi.CreateNoWindow = true;
+
+                    Process p = new Process();
+                    p.StartInfo = psi;
+                    p.OutputDataReceived += p_DataReceived;
+                    p.EnableRaisingEvents = true;
+                    p.Start();
+                    p.BeginOutputReadLine();
+                    p.WaitForExit();
+                }
+                
+                HandleVersion?.Invoke();
+            }
+
+            if (summary.result == BuildResult.Failed)
+            {
+                Debug.Log("Build failed");
+            }
+        }
+        
+        static void p_DataReceived(object sender, DataReceivedEventArgs e)
+        {
+            // Manipulate received data here
+            string MyPhones = e.Data.ToString();
+            UnityEngine.Debug.Log(MyPhones);
+        }
+    }
+}
+#endif
